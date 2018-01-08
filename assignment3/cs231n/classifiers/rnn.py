fecdda1788efdf5954545ac2e02cf12c289e6357
zadnik <jakub.zadnik.ext@nokia.com>
2018-01-08 15:27:45 +0200

    A3Q1: Implement RNN model loss and gradient

--- assignment3/cs231n/classifiers/rnn.py
+++ assignment3/cs231n/classifiers/rnn.py
@@ -137,7 +137,53 @@ class CaptioningRNN(object):
         # defined above to store loss and gradients; grads[k] should give the      #
         # gradients for self.params[k].                                            #
         ############################################################################
-        pass
+        
+        # Forward
+        # (1)
+        h0, h0_cache = affine_forward(features, W_proj, b_proj)
+        
+        # (2)
+        x, x_cache = word_embedding_forward(captions_in, W_embed)
+        
+        # (3)
+        if self.cell_type == 'rnn':
+            h, h_cache = rnn_forward(x, h0, Wx, Wh, b)
+        # elif self.cell_type == 'lstm:'
+        else:
+            raise ValueError("Invalid cell type: {}".format(self.cell_type))
+            
+        # (4)
+        scores, scores_cache = temporal_affine_forward(h, W_vocab, b_vocab)
+        
+        # (5)
+        loss, dscores = temporal_softmax_loss(scores, captions_out, mask, verbose=False)
+        
+        # Backward
+        # (4)
+        dh, dW_vocab, db_vocab = temporal_affine_backward(dscores, scores_cache)
+        
+        # (3)
+        if self.cell_type == 'rnn':
+            dx, dh0, dWx, dWh, db = rnn_backward(dh, h_cache)
+        # elif self.cell_type == 'lstm:'
+        else:
+            raise ValueError("Invalid cell type: {}".format(self.cell_type))
+            
+        # (2)
+        dW_embed = word_embedding_backward(dx, x_cache)
+        
+        # (1)
+        _, dW_proj, db_proj = affine_backward(dh0, h0_cache)
+        
+        grads['W_proj'] = dW_proj
+        grads['b_proj'] = db_proj
+        grads['W_embed'] = dW_embed
+        grads['Wx'] = dWx
+        grads['Wh'] = dWh
+        grads['b'] = db
+        grads['W_vocab'] = dW_vocab
+        grads['b_vocab'] = db_vocab
+        
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
